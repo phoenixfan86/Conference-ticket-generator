ec0c8152ee0f1b273f47c769390fa411b2f8b843
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-02 15:51:26 +0200

    add check email

--- js/ticket.js
+++ js/ticket.js
@@ -57,15 +57,39 @@ uploadArea.addEventListener('click', () => {
 	}
 })
 
-const inputName = document.getElementById('name')
-const inputEmail = document.getElementById('email')
-const inputGithub = document.getElementById('username')
 const submitBtn = document.getElementById('submit')
-const ownerEmail = document.getElementById('owner-email')
-const ownerName = document.getElementById('owner-name')
-const gitUsername = document.getElementById('github-username')
+const emailInput = document.getElementById('email')
+const emailError = document.getElementById('email-error')
 const ticketNumber = document.getElementById('ticket-number')
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+emailInput.addEventListener('input', () => {
+	const inputEmail = emailInput.value.trim()
+
+	if (!emailRegex.test(inputEmail)) {
+		emailError.classList.toggle('visible')
+	} else {
+		emailError.style.display = 'none'
+	}
+})
 
 submitBtn.addEventListener('click', function () {
 	document.querySelector('.ticket-wrapper').classList.toggle('visible')
+
+	const ownerName = document.getElementById('owner-name')
+	const ticketName = document.getElementById('ticket-name')
+	const ownerEmail = document.getElementById('owner-email')
+	const gitUsername = document.getElementById('github-username')
+
+	const inputName = document.getElementById('name').value.trim()
+	const inputEmail = document.getElementById('email').value.trim()
+	const inputGithub = document.getElementById('username').value.trim()
+
+	const randomNumber = Math.floor(10000 + Math.random() * 90000)
+	ticketNumber.textContent = `#${randomNumber}`
+	ownerName.textContent = inputName
+	ticketName.textContent = inputName
+	ownerEmail.textContent = inputEmail
+	gitUsername.textContent = inputGithub
 })
