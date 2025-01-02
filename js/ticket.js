bf343bdee5940f45ed2dd806d560e53da38f8524
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-02 16:07:26 +0200

    change script

--- js/ticket.js
+++ js/ticket.js
@@ -3,31 +3,27 @@ const fileInput = document.getElementById('file-input')
 const preview = document.getElementById('preview')
 const ticketImg = document.getElementById('ticket-img')
 
-// Функція для оновлення прев'ю
 function updatePreview(file) {
 	const reader = new FileReader()
 	reader.onload = e => {
-		preview.src = e.target.result // Оновлення зображення
+		preview.src = e.target.result
 		ticketImg.src = e.target.result
 	}
 	reader.readAsDataURL(file)
 }
 
-// Обробка події вибору файлу
 fileInput.addEventListener('change', event => {
 	const file = event.target.files[0]
 	if (file) {
 		if (file.size <= 500 * 1024) {
-			// Перевірка на розмір
 			updatePreview(file)
 		} else {
 			alert('Please upload a valid JPG or PNG file (max size: 500KB).')
 		}
-		fileInput.value = '' // Очищення для повторного вибору того ж файлу
+		fileInput.value = ''
 	}
 })
 
-// Обробка drag & drop
 uploadArea.addEventListener('dragover', event => {
 	event.preventDefault()
 	uploadArea.classList.add('dragover')
@@ -42,17 +38,14 @@ uploadArea.addEventListener('drop', event => {
 	uploadArea.classList.remove('dragover')
 	const file = event.dataTransfer.files[0]
 	if (file && file.size <= 500 * 1024) {
-		// Перевірка на розмір
 		updatePreview(file)
 	} else {
-		alert('Please upload a valid JPG or PNG file (max size: 500KB).')
+		alert('File too large.Please upload a photo under 500KB')
 	}
 })
 
-// Клік по області для відкриття діалогу вибору файлу
 uploadArea.addEventListener('click', () => {
 	if (!fileInput.matches(':focus')) {
-		// Перевірка, чи input активний
 		fileInput.click()
 	}
 })
@@ -68,9 +61,9 @@ emailInput.addEventListener('input', () => {
 	const inputEmail = emailInput.value.trim()
 
 	if (!emailRegex.test(inputEmail)) {
-		emailError.classList.toggle('visible')
+		emailError.classList.add('visible')
 	} else {
-		emailError.style.display = 'none'
+		emailError.classList.remove('visible')
 	}
 })
 
